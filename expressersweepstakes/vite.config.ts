e8c1e989da36dfddf7493b4e3675081e1b1cd87a
Flint <flintsmith@gmail.com>
2025-07-23 17:05:17 -0500

    Refactor Vite config to conditionally set output directory based on command

--- expressersweepstakes/vite.config.ts
+++ expressersweepstakes/vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
@@ -15,8 +15,8 @@ export default defineConfig({
   },
   base: '/',
   build: {
-    outDir: '../',
+    outDir: command === 'build' ? '../' : 'dist',
     assetsDir: 'assets',
     emptyOutDir: false,
   },
-})
+}))
